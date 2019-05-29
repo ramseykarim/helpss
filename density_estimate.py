7025fbc0d0761e574ebe39f5ac17d79b26601c80
Ramsey Karim <rkarim@astro.umd.edu>
2019-05-29 15:50:07 -0400

    renovated density estimate to be much faster, rely on numpy

--- density_estimate.py
+++ density_estimate.py
@@ -5,10 +5,7 @@ if plotting_remotely:
 	matplotlib.use('Agg')
 SAVE_NAME = "Figure_X_current.png"
 import matplotlib.pyplot as plt
-from matplotlib.patches import Patch
 from astropy.io import fits
-from planck_mask import gen_hist_and_stats
-import manticore_results as mtc
 import sys
 
 def show_plot():
@@ -17,72 +14,59 @@ def show_plot():
 	else:
 		plt.show()
 
+# # Department directories
 # working_dir = "/n/sgraraid/filaments/data/TEST4/helpss_scratch_work/"
-working_dir = "/home/ramsey/Documents/Research/Filaments/"
 # soln = "T4-absdiff-Per1J-3param-plus046-full.fits"
+# # Local directories
+working_dir = "/home/ramsey/Documents/Research/Filaments/"
 soln = "T4-absdiff-Per1J-plus045-pow-1000-0.1-1.80.fits"
 
-T = fits.getdata(working_dir + soln, 3)
-# mask = mtc.get_filament_mask()
-# T[~mask] = 0
-# T = np.log(T)
-# T -= np.nanmin(T)
-# T += 1
-# T = np.exp(T)
-T[np.isnan(T)] = 0
-
-Tflat = T.flatten(order='C')
-Tsort_i = np.argsort(Tflat)
-Tsort = Tflat[Tsort_i]
-
-# plt.figure(figsize=(11, 8.5))
-# plt.subplot(121)
-
-# new_T = Tflat.reshape(T.shape)
-cumulative_Tsort = np.cumsum(Tsort)
-CDF_Tsort = cumulative_Tsort / cumulative_Tsort[-1]
-PDF_Tsort = Tsort / cumulative_Tsort[-1]
-## Plot the CDF
-# plt.plot(Tsort, CDF_Tsort, '.')
-# show_plot()
-## Proof of rebuilding image from flattened array
-# empty_map = np.empty(T.shape)
-# for i, val in zip(Tsort_i, PDF_Tsort):
-#	 empty_map[i // T.shape[1], i % T.shape[1]] = val
-# plt.imshow(empty_map, origin='lower')
-# plt.colorbar()
-
+# Load image
+img = fits.getdata(working_dir + soln, 3)
+img[np.isnan(img)] = 0
+# Rescale
+img[img > 0] = np.sqrt(img[img > 0])
+# img[img > 0] -= np.min(img[img > 0])
+# Flatten
+img_flat = img.flatten(order='C')
+cumulative_img = np.cumsum(img_flat)
+cdf = cumulative_img / cumulative_img[-1]
+print("N0", np.min(img_flat[img_flat > 0]))
+print("N1", np.max(img_flat[img_flat > 0]))
+print("SNR N1/N0", np.max(img_flat[img_flat > 0])/np.min(img_flat[img_flat > 0]))
+print("sum(N)", cumulative_img[-1])
+print("ratio N0/sum(N)", np.min(img_flat[img_flat > 0])/cumulative_img[-1])
+print("ratio N1/sum(N)", np.max(img_flat[img_flat > 0])/cumulative_img[-1])
+print("ratio 5e22/sum(N)", np.sqrt(5e22)/cumulative_img[-1])
+print("n", cdf.size)
+best_n_points = int(cumulative_img[-1]*3*3/(9*np.sqrt(1e20)))
+print("best n", best_n_points)
+# pdf = img_flat / cumulative_img[-1]
 
 def get_position(i):
 	# Assume the center of the pixel
-	row = i // T.shape[1]
-	col = i % T.shape[1]
-	return np.array([col+0.5, row+0.5])
+	row = i // img.shape[1]
+	col = i % img.shape[1]
+	return np.concatenate([col+0.5, row+0.5], axis=1)
 
-def sample_kernel(sigma=1.0):
+def sample_kernel(size=1, sigma=1.0):
 	# Return two samples from a 2D gaussian kernel
-	return np.random.normal(scale=sigma, size=2)
+	return np.random.normal(scale=sigma, size=(size, 2))
 
-# plt.imshow(T, origin='lower')
-# plt.subplot(122)
-n_points = 7000
+n_points = 10000
+n_points = best_n_points
 kernel_width = 3 # pixels
-points = []
-for i in range(n_points):
-	rand_cdf_value = np.random.uniform()
-	chosen_cdf_value = np.min(CDF_Tsort[CDF_Tsort > rand_cdf_value])
-	rand_idx = np.where(chosen_cdf_value == CDF_Tsort)
-	rand_i = Tsort_i[rand_idx][0]
-	# x, y ordering is column, row
-	xy = get_position(rand_i)
-	xy += sample_kernel(sigma=kernel_width)
-	points.append(xy)
-points = np.array(points)
-# plt.plot(points[:, 0], points[:, 1], ',', markersize=3)
-# plt.xlim([0, T.shape[1]])
-# plt.ylim([0, T.shape[0]])
+rand_cdf_values = np.random.uniform(size=(n_points, 1))
+pixel_indices = np.searchsorted(cdf, rand_cdf_values)
+points = get_position(pixel_indices) + sample_kernel(size=n_points, sigma=kernel_width)
+
+# plt.figure(figsize=(16, 9))
+# plt.plot(points[:, 0], points[:, 1], ',', markersize=3, alpha=0.05)
+# plt.xlim([0, img.shape[1]])
+# plt.ylim([0, img.shape[0]])
 # plt.gca().set_aspect('equal')
 # show_plot()
+
 working_dir = "/home/ramsey/Documents/Research/AlphaX/PyAlpha_drafting/test_data/"
 np.savetxt("{:s}filament{:04d}_sampleNH2_betah1.80.dat".format(working_dir, n_points),
 	 points)
