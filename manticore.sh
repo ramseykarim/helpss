3c6bc4f4dbd852bd12d19328d58e8462ec3086dd
Ramsey Karim <rkarim@astro.umd.edu>
2019-08-20 16:04:49 -0400

    made manticore bash wrapper a lot smarter re arguments

--- manticore.sh
+++ manticore.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # bash script to run manticore, updated
 # author: ramsey
 # this needs to be run on sgra (or another manticore-ready computer)
@@ -11,41 +12,100 @@ beta_c="2.10"
 T_hot="16.00"
 working_dir="$(pwd)/"
 n_param="3"
-log="mant_log.log"
 n_cores="5"
 region="Per1"
-single="full-${manticore_version}-Per1-pow-1000-0.1-1.80.fits"
 
-print_usage() {
-    printf "Usage:\n -h for beta_hot\n -c for beta_cold\n -T for hot temperature\n -d for directory\n"
+print_usage_exit() {
+    printf "manticore.sh: usage: ./manticore.sh [valid arguments]
+    -h help (prints this message and exits)
+    -x run (even if no arguments are present) (at least one argument is necessary to run)
+    -H halo dust model/law (decimal spectral index, or OH5, DL5, or DL3)
+        default -h 1.80
+    -C core dust model/law (decimal spectral index, or OH5, DL5, or DL3)
+        default -c 2.10
+    -T halo temperature in Kelvin (number) (only used if running 3 parameter)
+        default -T 16.00
+    -d working directory containing input data
+        NEEDS to end in '/'
+        default -d <current directory> ($(pwd)/)
+    -2 run 2-parameter (single-component) Manticore fit (no argument)
+        default (no flag) is 3 parameter
+    -l filename of log file
+        see pattern rules for -o
+        not overwritten, just appended to. created if does not exist.
+        default -l mant_log.log in working directory (-d)
+    -s filename of 2 parameter result (only used if running 3 parameter)
+        see pattern rules for -o
+        the file will only be read
+        default -s full-${manticore_version}-Per1-pow-1000-0.1-1.80.fits, in working directory (-d)
+    -t number of CPU threads to use (positive integer)
+        default -t 5
+    -o output filename
+        if ends in '/', assumes this specifies a write directory and uses default filename
+        if contains '/' but does not end in '/', assumes this specifies full file name and path to which to write
+        if not contains '/', assumes this specifies only the filename and writes to working directory
+        if not set, writes to default filename in working directory
+"
+    exit 1
 }
 
 # parse arguments
-while getopts 'h:c:T:d:2l:s:t:' flag; do
+while getopts 'hxH:C:T:d:2l:s:t:o:' flag ; do
     case "${flag}" in
-        h) beta_h="${OPTARG}" ;;
-        c) beta_c="${OPTARG}" ;;
+        h) print_usage_exit ;;
+        x) : ;;
+        H) beta_h="${OPTARG}" ;;
+        C) beta_c="${OPTARG}" ;;
         T) T_hot="${OPTARG}" ;;
-        d) working_dir="${OPTARG}" ;;
+        d) working_dir="${OPTARG}"
+            if [[ "$working_dir" != *\/ ]] ; then
+                printf "$0: invalid directory name -- d\n"
+                print_usage_exit
+            fi ;;
         2) n_param="2" ;;
         l) log="${OPTARG}" ;;
         s) single="${OPTARG}" ;;
         t) n_cores="${OPTARG}" ;;
-        *) print_usage
-            exit 1 ;;
+        o) out="${OPTARG}" ;;
+        *) print_usage_exit ;;
     esac
 done
-log="${working_dir}${log}"
 
-# format power laws if not using full dust model
-if [[ $beta_h != "DL3" ]] && [[ $beta_h != "OH5" ]] ; then
-    beta_h="pow-1000-0.1-${beta_h}"
-fi
-if [[ $beta_c != "DL3" ]] && [[ $beta_c != "OH5" ]] ; then
-    beta_c="pow-1000-0.1-${beta_c}"
+if [[ -z $1 ]] ; then
+    printf "$0: need at least one argument (-x to run with all defaults)\n"
+    print_usage_exit
 fi
 
-# put together some manticore arguments
+
+# A couple helper functions for parsing arguments
+parse_dust() {
+    # first/only argument is dust model argument
+    if [[ "$1" != "DL3" ]] && [[ "$1" != "DL5" ]] && [[ "$1" != "OH5" ]] ; then
+        echo "pow-1000-0.1-${1}"
+    else
+        echo "$1"
+    fi
+}
+
+parse_filename() {
+    # first arg is input
+    # second arg is default filename
+    # default directory is working directory
+    if [[ -z $1 ]] ; then
+        echo "${working_dir}$2"
+    elif [[ "$1" == *\/ ]] ; then
+        echo "${1}$2"
+    elif [[ "$1" != *\/* ]] ; then
+        echo "${working_dir}${1}"
+    else
+        echo "$1"
+    fi
+}
+
+# Format power laws if not using full dust model
+beta_h=$(parse_dust $beta_h)
+beta_c=$(parse_dust $beta_c)
+# Put together some manticore arguments depending on 2/3 parameter run
 if [[ "$n_param" == "3" ]] ; then
     dust="${beta_h},${beta_c}"
     Thstub="-Th${T_hot}"
@@ -53,6 +113,13 @@ else
     dust="${beta_h}"
     Thstub=""
 fi
+
+# Parse log argument
+log=$(parse_filename $log mant_log.log)
+# Parse single component argument
+single=$(parse_filename $single full-${manticore_version}-Per1-pow-1000-0.1-1.80.fits)
+# Parse output argument
+out=$(parse_filename $out full-${manticore_version}-${region}-${dust}${Thstub}.fits)
 
 # Print out a buch of status text
 echo "============================="
@@ -90,7 +157,6 @@ echo "using ${n_cores} cores on $(hostname) as $(whoami)" >> $log
 
 
 # Really put together the command line arguemnts
-out="${working_dir}full-${manticore_version}-${region}-${dust}${Thstub}.fits"
 manticore="/sgraraid/filaments/manticore-${manticore_version}/manticore"
 rccstub="-remapped-conv"
 fitsstub=".fits"
@@ -109,7 +175,7 @@ for b in $b2 $b3 $b4 ; do
   err_args="${err_args} -e ${b}${err}${serr_mod}${fitsstub}"
 done
 
-if [[ "${n_param}" == "3" ]] ; then
+if [[ "$n_param" == "3" ]] ; then
   call_command="${manticore} -3 -T ${T_hot} -s ${working_dir}${single}  -c 0.0 -g 100,100"
 else
   call_command="${manticore} -${n_param} -g 100"
