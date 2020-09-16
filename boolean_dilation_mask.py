4b9e868d4a10c9020a433ba1ae79fc32f9d8145a
Ramsey Karim <rkarim@astro.umd.edu>
2020-09-16 00:16:34 -0400

    documented and cleaned up the new bool dilation mask function

--- boolean_dilation_mask.py
+++ boolean_dilation_mask.py
@@ -24,116 +24,90 @@ Created: September 15, 2020
 __author__ = "Ramsey Karim"
 
 import numpy as np
-import matplotlib.pyplot as plt
-
-from astropy.io import fits
 
 
-def dilate(array, times=1, forbidden=None):
+def dilate(mask, forbidden):
     """
-    This is the old dilation routine from boolean_islands.py
-    I want to benchmark this against a new routine I write.
+    Dilate the 1s in mask into the 0s in mask. Never touch pixels that are
+    1 in forbidden. Dilation is a one-pixel shift into all of the 8 adjacent
+    available pixels.
 
-    I modified it to make a "forbidden" set out of a bool array where 1s are
-    forbidden
-    """
-    if times == 0:
-        return np.copy(array)
-    if forbidden is None:
-        forbidden = set()
-    else:
-        forbidden = set(zip(*np.where(forbidden)))
-    shape = array.shape
-    def valid_point(i, j):
-        within_bounds = not ((i < 0) or (j < 0) or (i >= shape[0]) or (j >= shape[1]))
-        not_forbidden = (i, j) not in forbidden
-        return within_bounds and not_forbidden
-    dilating_ones = set(zip(*np.where(array)))
-    new_ones = set()
-    for iteration in range(times):
-        totally_new_ones = set()
-        for i0, j0 in dilating_ones:
-            totally_new_ones.update(*(set((i0 + di, j0 + dj) for dj in range(-1, 2) if (di or dj) and valid_point(i0 + di, j0 + dj)) for di in range(-1, 2)))
-        totally_new_ones.difference_update(dilating_ones)
-        new_ones |= totally_new_ones
-        dilating_ones = totally_new_ones
-    new_array = np.copy(array).astype(bool)
-    new_array[tuple(zip(*new_ones))] = True
-    return new_array
+    Helper function for padded_mask
 
-
-def new_dilate(array, forbidden):
+    :param mask: np.ndarray of bool
+        1 where we dilate FROM, 0 where we dilate INTO
+    :param forbidden: np.ndarray of bool
+        1 where we should never work (NaN equivalent)
+        All 1s in mask should be 0 in forbidden
     """
-    Both arguments should be boolean
-    :param array: 1 where valid, 0 where invalid
-    :param forbidden: 1 where we should never work (NaN equivalent)
-        all array 1s should be 0 in forbidden
-    """
-    new_array = np.copy(array)
+    new_mask = np.copy(mask)
     # Shift right
-    new_array[:, 1:] |= array[:, :-1]
+    new_mask[:, 1:] |= mask[:, :-1]
     # Shift left
-    new_array[:, :-1] |= array[:, 1:]
+    new_mask[:, :-1] |= mask[:, 1:]
     # Shift up
-    new_array[1:, :] |= array[:-1, :]
+    new_mask[1:, :] |= mask[:-1, :]
     # Shift down
-    new_array[:-1, :] |= array[1:, :]
+    new_mask[:-1, :] |= mask[1:, :]
 
     # Shift up-right
-    new_array[1:, 1:] |= array[:-1, :-1]
+    new_mask[1:, 1:] |= mask[:-1, :-1]
     # Shift down-right
-    new_array[:-1, 1:] |= array[1:, :-1]
+    new_mask[:-1, 1:] |= mask[1:, :-1]
     # Shift down-left
-    new_array[:-1, :-1] |= array[1:, 1:]
+    new_mask[:-1, :-1] |= mask[1:, 1:]
     # Shift up-left
-    new_array[1:, :-1] |= array[:-1, 1:]
+    new_mask[1:, :-1] |= mask[:-1, 1:]
 
-    new_array[forbidden] = False
+    new_mask[forbidden] = False
 
-    return new_array
+    return new_mask
 
 
-def mask_from_old_dilation_routine(array, first_pad, second_pad):
+def padded_mask(array, first_pad, second_pad):
     """
-    Use the old dilation routine to make the mask as specified in this file's
-    header.
+    Masking function that selects a border around designated pixels.
+
+    The argument array should contain 1s, 0s, and NaNs. The 1s represent
+    pixels FROM which we dilate outwards. The 0s represent pixels INTO which
+    we dilate. The NaNs represent pixels we should never bother with; those NaNs
+    will be conserved in the resulting map, and no other NaNs will be created.
+
+    The border between 1s and 0s is expanded into the 0s, pixel by pixel, for
+    <first_pad> iterations. From there, it is expanded again in the same way
+    for <second_pad> iterations. All pixels that were added in the second set
+    of iterations are assigned 1 in the resulting mask array. All other pixels
+    are assigned 0, except for NaNs, which remain NaN.
+
+    :param array: np.ndarray of float
+        The input mask, represented as a 2D array of floats containing only
+        1s, 0s, and NaNs
+    :param first_pad: int
+        number of iterations (roughly equivalent to pixels) for the first
+        round of dilation
+    :param second_pad: int
+        number of iterations for the second round of dilation
+    :return: np.ndarray of float
+        Same format as the input array; 1s represent included in mask, 0s
+        represent excluded, and NaNs are conserved from the input.
+        The 1s represent regions between the boundaries of the first and second
+        dilations.
     """
+    # Split the array into two boolean arrays
+    # Make a NaN mask, 1 where NaN
     nan_mask = np.isnan(array)
-    valid_mask = (array == 1) # array == 0
-    first_round_mask = dilate(valid_mask, times=first_pad, forbidden=nan_mask)
-    second_round_mask = dilate(first_round_mask, times=second_pad, forbidden=nan_mask)
-    result = (second_round_mask & ~first_round_mask).astype(float)
-    result[nan_mask] = np.nan
-    return result
-
-
-def mask_from_new_dilation_routine(array, first_pad, second_pad):
-    """
-    Use the new array indexing shift dilation routine to make the mask as
-    specified in this file's header.
-    """
-    nan_mask = np.isnan(array)
-    first_round_mask = (array == 1) # array == 0
+    # Make a mask where array 1s are 1 and everything else (0 and NaN) is 0
+    first_round_mask = (array == 1)
+    # Dilate <first_pad> times
     for i in range(first_pad):
-        first_round_mask = new_dilate(first_round_mask, nan_mask)
+        first_round_mask = dilate(first_round_mask, nan_mask)
+    # Save this state of the mask
     second_round_mask = np.copy(first_round_mask)
+    # Dilate <second_pad> times
     for i in range(second_pad):
-        second_round_mask = new_dilate(second_round_mask, nan_mask)
+        second_round_mask = dilate(second_round_mask, nan_mask)
+    # Compare the first and second round dilations
     result = (second_round_mask & ~first_round_mask).astype(float)
+    # Conserve NaNs
     result[nan_mask] = np.nan
     return result
-
-
-if __name__ == "__main__":
-    arr = fits.getdata("../syp_mask_3layer.fits")
-    # newmask = mask_from_old_dilation_routine(arr, 5, 20)
-    newmask = mask_from_new_dilation_routine(arr, 5, 20)
-
-    """
-    The new method is vasly faster. Array shifting!
-    """
-
-    fig, axes = plt.subplots(1, 2, sharex=True, sharey=True)
-    axes[0].imshow(arr, origin='lower')
-    axes[1].imshow(newmask, origin='lower')
-    plt.show()
