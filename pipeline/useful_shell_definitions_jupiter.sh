e11303100269dd37d34a5183e1bc8b5db8123ec1
Ramsey Karim <rkarim@astro.umd.edu>
2021-05-26 23:48:37 -0400

    adjusted the shell definitions files and made one for jupiter specifically

---
+++ pipeline/useful_shell_definitions_jupiter.sh
@@ -0,0 +1,19 @@
+# can just "run" this file to set up the calibration environment
+# same as useful_shell_definitions_sgra, run with . into terminal
+
+# append this directory to PATH so we can use calibrate.py and such
+PATH=$(pwd):$PATH
+# activate the healpy conda environment
+source activate py36
+
+# useful for printing the Planck beta files from a given set of regions
+print_beta () {
+  file_list=$(ls ${1}Herschel/processed/*/beta-stats.txt)
+  for i in $file_list ; do
+    echo $i
+    echo "-----------"
+    cat $i
+    echo "-----------"
+    echo "-----------"
+  done
+}
